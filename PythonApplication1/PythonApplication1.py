a790db8af6aaf30ef94402d9f4857eb25d1bda3c
Chen Yu Pao <windperson@gmail.com>
2014-05-26 09:39:37 +0800

    add simplified Chinese test

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 import sys
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
+
 first = file(sys.argv[1],'r')
 second = file(sys.argv[2], 'w')
 print u'輸入檔案為:'.encode('cp950'), '\'%s\''%sys.argv[1]
-print '输入档案为:'.encode('gbk'), '\'%s\''%sys.argv[1]
+print u'输入档案为:'.encode('gbk'), '\'%s\''%sys.argv[1]
 print '\n'
 print u"輸出檔案為:".encode("cp950"), "\"{}\"".format(sys.argv[2])
-print "输出档案为:".encode("gbk"), "\"{}\"".format(sys.argv[2]) 
+print u"输出档案为:".encode("gbk"), "\"{}\"".format(sys.argv[2]) 
 for line in first:
     second.write(line)
 first.close()
